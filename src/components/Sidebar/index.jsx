12649c5ac840f3cd4f5fa12889835c87cf882a4b
Ryan adonis <rr1294989@gmail.com>
2024-01-11 07:22:05 +0100

    icon-end

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,9 +1,11 @@
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import codechefLogo from '../../assets/images/codechef.png'
-import LogoSubtitle from '../../assets/images/Sudip_logo_white.png'
+import Linkedin from '../../assets/images/linkedin.png'
+import Download from '../../assets/images/circle-down-regular.svg'
+import Git from '../../assets/images/icons8-github-64.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 import {
   faHome,
   faUser,
@@ -19,32 +21,42 @@ const Sidebar = () => {
   return (
     <>
       <div className="nav-bar">
-        {/* <Link className="logo" to="/">
-          <img src={LogoS} alt="Logo" />
-          <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
-        </Link> */}
+        <Link   to="/">
+          <img src={LogoS} style={{display : 'flex'  , margin : '5px' ,  marginBottom : '10px',  width : '50px' , height : '50px' , borderRadius : '50px'}}  alt="Logo" />
+        </Link>
      
         <nav>
-          <p  className="wrapper" style={{display : 'block' , marginTop : '-440px' }}>
-          <div class="button">
-        <div class="icon">
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" style={{width : '15px' , height : '15px'}} />
-        </div>
-        <span>Gmail</span>
-      </div>
-      <div class="button">
-        <div class="icon">
-        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-        </div>
-        <span>Github</span>
-      </div>
-      <div class="button">
-        <div class="icon">
-        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-        </div>
-        <span>Linkedin</span>
-      </div>
+          <p style={{display : 'block' , marginTop : '-440px' }}>
 
+          <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/sudip-banerjee-300b691bb/"
+              target="_blank"
+              rel="noreferrer"
+            >
+                <img src={Linkedin} width="24" alt="Codechef" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/metal-oopa"
+              target="_blank"
+              rel="noreferrer"
+            >
+                    <img src={Git} width="24" alt="Codechef" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codechef.com/users/metal_oopa"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={codechefLogo} width="24" alt="Codechef" />
+            </a>
+          </li>
+        </ul>
 
           </p>
           
@@ -85,9 +97,10 @@ const Sidebar = () => {
               href="https://www.linkedin.com/in/sudip-banerjee-300b691bb/"
               target="_blank"
               rel="noreferrer"
+              
             >
-              Doawnload PDF
-              <FontAwesomeIcon icon={faLinkedin} color="#b9b9b9" />
+              
+                  <img src={Download} width="24" alt="Codechef" />
             </a>
           </li>
 
